16e0181fbd4cafcdb31201be57c0322661865249
Nicole Nunes <nuneskn@appstate.edu>
2023-04-20 11:04:17 -0400

    Introducing text file

--- MakeADecision.java
+++ MakeADecision.java
@@ -1,8 +1,27 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class MakeADecision {
-    public static void main(String args[]){
+    public static void main(String args[]){	
+	    File text = new File("TypesOfFood.txt");
+	    try {
+	    	Scanner scan = new Scanner(text);
+	    	int lineNumber = 1;
+	    	while(scan.hasNextLine()){
+	    		String line = scan.nextLine();
+			System.out.println("line " + lineNumber + " :" + line);
+			lineNumber++;
+	    	}
+		scan.close();
+	    }
+	    catch (FileNotFoundException e){
+	    	e.printStackTrace();
+	    }
+
 	    ArrayList<String> possible = new ArrayList<String>();
 	    possible.add("switch");
 	    possible.add("pc");
